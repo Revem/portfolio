77161578861e302182b99ece7e42a3cfa95f07b0
Victor Picon <revemvi@hotmail.com>
2023-10-04 19:13:45 -0300

    feat: footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,11 @@
-export default function Footer() {
+export default function Footer({ lang }) {
   return (
-    <div className="text-center"><h1>
-      feito com <span className="text-victorViolet"> ♥ </span>por <span className="text-victorViolet">Victor</span> Picon
-    </h1></div>
+    <div className="text-center">
+      {lang == 'pt-br' ? <h1>
+        feito com <span className="text-victorViolet"> ♥ </span>por <span className="text-victorViolet">Victor</span> Picon
+      </h1> : <h1>
+        Made with <span className="text-victorViolet"> ♥ </span>by <span className="text-victorViolet">Victor</span> Picon
+      </h1>}
+    </div>
   )
 }
