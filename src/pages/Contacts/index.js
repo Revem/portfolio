969b302ef38340b7c5d5e1c7b64bca3eabc879d2
Victor Picon <revemvi@hotmail.com>
2023-10-05 18:57:53 -0300

    fix: responsive

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,4 @@
+'use client'
 import { motion, useAnimation } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { sendContactForm } from "../../lib/api";
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,3 +1,4 @@
+'use client'
 import Card from '@/components/Card';
 import { motion, useAnimation } from 'framer-motion';
 import { useEffect, useState } from 'react';
