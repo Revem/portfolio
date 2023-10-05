81b5d8efc9bef47438d5f2b7fe889d53cca3dba6
Victor Picon <revemvi@hotmail.com>
2023-10-05 19:00:01 -0300

    fix: window error

--- src/pages/Tecnologies/index.js
+++ src/pages/Tecnologies/index.js
@@ -11,23 +11,29 @@ export default function Tecnologies({ lang }) {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 200) {
-        if (!isVisible) {
-          setIsVisible(true);
-          controls.start({ opacity: 1, x: 0 });
-        }
-      } else {
-        if (isVisible) {
-          setIsVisible(false);
-          controls.start({ opacity: 0, x: -50 });
+      if (typeof window !== 'undefined') { // Verifica se o window está definido
+        if (window.scrollY > 200) {
+          if (!isVisible) {
+            setIsVisible(true);
+            controls.start({ opacity: 1, x: 0 });
+          }
+        } else {
+          if (isVisible) {
+            setIsVisible(false);
+            controls.start({ opacity: 0, x: -50 });
+          }
         }
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    if (typeof window !== 'undefined') { // Verifica se o window está definido
+      window.addEventListener('scroll', handleScroll);
+    }
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      if (typeof window !== 'undefined') { // Verifica se o window está definido
+        window.removeEventListener('scroll', handleScroll);
+      }
     };
   }, [controls, isVisible]);
 
